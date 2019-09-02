1cfd705324ba9c8ef7f8cf23c6319643f6467fbb
Suin Kim <sk3665@drexel.edu>
2019-09-02 14:17:33 -0400

    Node.js server

--- server.js
+++ server.js
@@ -1,8 +1,39 @@
 //Node.js server file
 
 var express = require('express')
+var fs = require("fs")
 var mysql = require('mysql')
+var bodyParser = require("body-parser")
+
 var app = express()
+var password = fs.readFileSync("./password.txt", "utf8")
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: password,
+    database: "hw5"
+})
+
+app.use(express.static("."))
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+con.connect(function (err) {
+    if (err) {
+        console.log("Error connecting to database" + err)
+    }
+    else {
+        console.log("Database successfully connected")
+    }
+})
+
+app.get("/mastery", function(req,res) {
+    return
+})
+
+app.get("/getMastery", function(req,res) {
+    return
+})
 
 app.get('/home', function (req,res) {
     res.redirect("./sk3665_HW5.html")
